d35d2da1625a8e90c7a210eae357435ccfb9aa3e
amel <amal.mehalaine@outlook.fr>
2024-02-28 11:52:34 +0100

    add control prompt

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import sys
 # ! we verify that the environment variables are loaded
 # print(os.getenv('OPENAI_API_KEY'))
 
 from langchain_openai import ChatOpenAI
+# this import allows us to format prompts with variables
 from langchain.prompts import PromptTemplate
-
 
 # we create an instance of Chat-GPT programmatically
 chat_gpt = ChatOpenAI()
@@ -16,10 +17,35 @@ control_prompt_template = """Do you have the answer to the question below, delim
 {question}
 -----------------------------------
 If the answer is yes, give us your source."""
-initial_prompt = "What is LangChain and what can I do with it?"
 
-prompt = PromptTemplate.from_template(control_prompt_template)
-control_prompt = prompt.format(question=initial_prompt)
+formatting_prompt_template = """You are a bot that always outputs True or False.
+You are given a question in natural language, delimited by dashes below.
+-----------------------------------
+{question}
+-----------------------------------
+You are also given an answer in natural language, delimited by dashes below.
+-----------------------------------
+{answer}
+-----------------------------------
+You must output False if the answer says something like "I don't know the answer".
+Only output the boolean, nothing else."""
 
-res = chat_gpt.invoke(control_prompt)
-print(res)
+# we get the question from the command line
+question = sys.argv[1]
+
+# we check if the bot knows the answer to the question
+knowledge_base_prompt = PromptTemplate.from_template(control_prompt_template)
+control_prompt = knowledge_base_prompt.format(question=question)
+knowledge_base_answer = chat_gpt.invoke(control_prompt)
+
+# we format the answer in a JSON object
+formatting_prompt = PromptTemplate.from_template(formatting_prompt_template)
+formatting_response = chat_gpt.invoke(formatting_prompt.format(
+    question=question,
+    answer=knowledge_base_answer
+))
+
+if (formatting_response.content == "True"):
+    print(knowledge_base_answer)
+else:
+    print("TODO make a web search to find the answer to the question.")
